090827b8ab806e6e206b902df1bd6a541c49fbf5
Rasim Hait <pc.sinner@gmail.com>
2024-08-18 02:04:38 +0300

    Migration SQL fix

--- sql/updates/hotfixes/2024_08_18_02_implement_spell_visual_kit_model_attach.sql
+++ sql/updates/hotfixes/2024_08_18_02_implement_spell_visual_kit_model_attach.sql
@@ -1,25 +1,6 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : Local_10.2.5
- Source Server Type    : MySQL
- Source Server Version : 80034 (8.0.34)
- Source Host           : localhost:3306
- Source Schema         : hotfixes
-
- Target Server Type    : MySQL
- Target Server Version : 80034 (8.0.34)
- File Encoding         : 65001
-
- Date: 25/03/2024 20:15:06
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
--- ----------------------------
--- Table structure for spell_visual_kit_model_attach
--- ----------------------------
 DROP TABLE IF EXISTS `spell_visual_kit_model_attach`;
 CREATE TABLE `spell_visual_kit_model_attach`  (
   `ID` int UNSIGNED NOT NULL DEFAULT 0,
@@ -48,9 +29,11 @@ CREATE TABLE `spell_visual_kit_model_attach`  (
   `LowDefModelAttachID` int NOT NULL DEFAULT 0,
   `StartDelay` float NOT NULL DEFAULT 0,
   `Unknown901` float NOT NULL DEFAULT 0,
+  `Unknown1100` int NOT NULL DEFAULT 0,
   `ParentSpellVisualKitID` int NOT NULL DEFAULT 0,
   `VerifiedBuild` int NOT NULL DEFAULT 0,
   PRIMARY KEY (`ID`, `VerifiedBuild`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
 
 SET FOREIGN_KEY_CHECKS = 1;
