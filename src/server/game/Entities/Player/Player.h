b84c2af69cfc8f2c19ae20259a98bbb48e14517b
Rasim Hait <pc.sinner@gmail.com>
2024-08-17 06:30:13 +0300

    Effect system + Initial files

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -3368,8 +3368,10 @@ class TC_GAME_API Player final : public Unit, public GridObject<Player>
         bool ProcessItemCast(SpellCastRequest& castRequest, SpellCastTargets const& targets);
         bool CanExecutePendingSpellCastRequest();
 
-    public:
+#pragma region Noblegarden
+     public:
         inline static std::function<void(Player*, Unit*)> OnMeetUnit;
+#pragma endregion
 };
 
 TC_GAME_API void AddItemsSetItem(Player* player, Item const* item);
